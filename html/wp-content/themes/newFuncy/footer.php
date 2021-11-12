9ab26862eefa069480a3fe78c6892fced120aaa1
Alex <afontanmoreira@danielcastelao.org>
2021-11-12 11:35:53 +0100

    Initial commit

---
+++ html/wp-content/themes/newFuncy/footer.php
@@ -0,0 +1,4 @@
+<h1>
+    mi footer
+</h1>
+<?php
---
+++ html/wp-content/themes/newFuncy/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>New Funcy</title>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+
+<body>
+
+<?php get_header() ?>
+
+
+<div id="cuerpo">
+    <?php
+    echo "<h1>Hola Funcy</h1>"
+    ?>
+</div>
+<?php get_footer() ?>
+</body>
+</html>
